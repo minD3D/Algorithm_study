8ddcf21f8b80d0c3945df18fbe65208f6b868bd3
minD3D <gomdori626@gmail.com>
2018-12-28 19:58:17 +0900

    12/28

--- 3184_sheep.cpp
+++ 3184_sheep.cpp
@@ -1,48 +1,88 @@
 #include <iostream>
-#include <map>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <string>
 
 using namespace std;
-int r,c;
-int arr[251][251];
-int visit[251][251];
+int row,col;
+int board[251][251];
+bool visit[251][251];
 vector<pair<int,int>> t;
+
 int dr[4]={0,0,1,-1};
 int dc[4]={1,-1,0,0};
-int o=0, int v=0;
-void bfs(int tr, int tc){j
-    for(int i=0; i<4; i++){
+int totalo=0, totalv=0;
+
+void bfs(int r, int c){
+    queue<pair<int,int>> q;
+    q.push(make_pair(r,c));
+    int cnto=0,cntv=0,cntTotal=0;
+    visit[r][c]=true;
+    while(!q.empty()){
+        int tr=q.front().first;
+        int tc=q.front().second;
+        q.pop();
+        
+        if(board[tr][tc]==1)
+            cnto+=1;
+        if(board[tr][tc]==2)
+            cntv+=1;
+
+        // cout<<tr<<tc<<cnto<<cntv<<endl;
+
+        cntTotal+=1;
+
+        for(int i=0; i<4; i++){
         int nr=tr+dr[i];
-        int nc=tr+dc[i];
-        if(nr<r&&nc<c&&nr>=0&&nc>=0){
-            if(arr[nr][nc]==1&&visit[nr][nc]!=1)
-            
-        }
+        int nc=tc+dc[i];
+            if(nr<row&&nc<col&&nr>=0&&nc>=0){
+                if(board[nr][nc]!=-1&&!visit[nr][nc]){
+                    q.push(make_pair(nr,nc));
+                    visit[nr][nc]=true;
+                }
+            }
+        }    
     }
+    // for(int i=0; i<row; i++){
+    //     for(int j=0; j<col; j++){
+    //         cout<<visit[i][j];
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<cnto<<cntv<<endl;
+    if(cnto>cntv){
+        totalo+=cnto;
+    }
+    else{
+        totalv+=cntv;
+    }
+    return;
 }
 
 int main(){
-    cin>>r>>c;
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
+    cin>>row>>col;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             char a;
             cin>>a;
-            if(a=='.')arr[i][j]=0;
-            if(a=='#')arr[i][j]=-1;
-            if(a=='o'){
-                arr[i][j]=1;
+            if(a=='.')board[i][j]=0;
+            if(a=='#')board[i][j]=-1;
+            if(a=='o'){//양
+                board[i][j]=1;
                 t.push_back(make_pair(i,j));
             }
-            if(a=='v'){
-                arr[i][j]=2;
+            if(a=='v'){//늑대
+                board[i][j]=2;
                 t.push_back(make_pair(i,j));
             }
         }
     }
 
     for(int i=0; i<t.size(); i++){
-        bfs(t[i].first,t[i].second);
+        if(!visit[t[i].first][t[i].second]){
+            bfs(t[i].first,t[i].second);
+        }
     }
+    cout<<totalo<<" "<<totalv<<endl;
 }
