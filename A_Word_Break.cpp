c3433811fb1de7ea23dbae33be6eb93864639458
Minji Kim <gomdori626@gmail.com>
2019-02-05 13:23:38 +0100

    2/4

--- A_Word_Break.cpp
+++ A_Word_Break.cpp
@@ -1,15 +1,45 @@
+#include<string>
+#include<vector>
+#include<map>
+using namespace std;
+
+//time limit error
 class Solution {
 public:
-    int numJewelsInStones(string J, string S) {
-        int res=0;
-        for(int i=0; i<S.length(); i++){
-            for(int j=0; j<J.length(); j++){
-                if(S[i]==J[j]){
-                    res+=1;
-                    break;
+    map<char,vector<string>> dictionary;
+    bool res;
+    string str;
+    
+    //match function
+    bool matchWord(int idx){
+        if(idx==str.length()){
+            res=true;
+            return true;
+        }
+        char fc=str[idx];
+        for(int i=0;i<dictionary[fc].size(); i++){
+            string cpr_str=dictionary[fc][i];
+            string sub_str=str.substr(idx,cpr_str.length());
+            if(cpr_str==sub_str){
+                if(matchWord(idx+cpr_str.length())){
+                    return true;
                 }
             }
         }
+        return false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        str=s;
+        //mapping at dictionary
+        for(int i=0; i<wordDict.size(); i++){
+            if(wordDict[i].length()==0)
+                continue;
+            char firstLetter=wordDict[i][0];
+            dictionary[firstLetter].push_back(wordDict[i]);
+        }
+        res=false;
+        matchWord(0);
         return res;
     }
 };
