065247b0843d36b2f73963e6526268faf78811b1
Minji Kim <gomdori626@gmail.com>
2019-02-07 17:34:31 +0100

    2/7

---
+++ A_Sliding_Window_Max.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& num, int k) {
+        vector<int> res;
+        vector<int> nums;
+        nums=num;
+        if(nums.size()==0)return res;
+        vector<pair<int,int>> order;
+        for(int i=0; i<nums.size(); i++){
+            order.push_back(make_pair(-1*nums[i], i));
+        }
+        sort(order.begin(),order.end());
+        
+        for(int i=0; i<nums.size()-k+1; i++){
+            int pnt=0;
+            while(order[pnt].second>=i+k||i>order[pnt].second){
+                pnt++;
+            }
+            res.push_back(-1*order[pnt].first);
+        }
+        
+        return res;
+    }
+};
